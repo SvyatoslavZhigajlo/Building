2a94012f8d791faa76f041b41cebb10a034bd8bd
Svyatoslav Zhigajlo <s.zhigajlo@gmail.com>
2021-10-24 21:06:44 +0300

    Базовый класс - Здания!

---
+++ src/Building.java
@@ -0,0 +1,9 @@
+public class Building {
+
+    private String foundation;//фундамент
+    private int numberFloors; //кол-во этажей
+    private int ceilingHeight; //cm
+    private float builtUpArea; //площадь застройки
+    private String[] communications; // коммуникации
+    private String[] commonAreas; //места общего пользования
+}
